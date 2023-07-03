898a09f18d5c44d263bc2aa131dc382bbd17e7bc
Abdelrahman Mustafa <Abdelrahman4983@gmail.com>
2023-07-03 14:41:42 +0300

    Last Update

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,5 +1,4 @@
-from pipeline import clean
-from pipeline import tokenizer
+from pipeline import clean, tokenizer
 import pickle
 from translate import Translator
 
@@ -10,6 +9,6 @@ with open("pipeline/encoder", "rb") as f:
 
 translator = Translator(from_lang='arabic', to_lang='english')
 
+
 def translate(text):
     return translator.translate(text)
-
