f61877f776bf7a4f6751f15972240745df445697
Abdelrahman Mustafa <Abdelrahman4983@gmail.com>
2023-07-03 23:17:38 +0300

    wsgi.py added

--- api.py
+++ api.py
@@ -1,22 +1,15 @@
 from flask import Flask, jsonify, request
 from detector import Detector
-from pipeline import translate
 import easyocr
-from translate import Translator
 import os 
 
 os.environ["_BARD_API_KEY"]= 'YQhd-K3VoTIJtrpJdi4H7WFih4uI_veFBYjAbLPaYwOVe-emstt5CvSOxY1TqAmADKGFoA.'   
 model = Detector()
 ocr = easyocr.Reader(['en','ar'])
 
-translator = Translator(from_lang='arabic', to_lang='english')
-
-def translate(text):
-    return translator.translate(text)
-
 app = Flask(__name__)
 
-@app.post('/english')
+@app.post('/predict')
 def english():
     data = request.json
     try:
@@ -32,24 +25,7 @@ def english():
         result = jsonify({'error' : str(e)})
     return result
 
-@app.post('/arabic')
-def arabic():
-    data = request.json
-    try:
-        sample = data['text']
-
-    except KeyError:
-        return jsonify({'error' : 'No text sent'})
-    
-    predictions = model.predict(sample)
-
-    try:
-        result = jsonify(predictions)
-    except TypeError as e:
-        result = jsonify({'error' : str(e)})
-    return result
-
-@app.post('/english-image')
+@app.post('/predict-image')
 def english_image():
     data = request.files['img']
     try:
@@ -68,28 +44,3 @@ def english_image():
     except TypeError as e:
         result = jsonify({'error' : str(e)})
     return result
-
-
-@app.post('/arabic-image')
-def arabic_image():
-    data = request.files['img']
-    try:
-        extracted_text = ocr.readtext(data.stream.read(), detail=0, paragraph=True)
-    except KeyError:
-        return jsonify({'error' : 'No text sent'})
-        
-    text=''
-    for i in extracted_text:
-        text+=i
-    
-    predictions = model.predict(text)
-    
-    try:
-        result = jsonify(predictions)
-    except TypeError as e:
-        result = jsonify({'error' : str(e)})
-    return result
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
