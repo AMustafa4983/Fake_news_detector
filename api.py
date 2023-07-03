afd52964d28e7f97ed68a2ebf5f8cfdda32f5561
Abdelrahman Mustafa <Abdelrahman4983@gmail.com>
2023-07-03 12:18:05 +0300

    Arabic Lang, Image detection features added

--- api.py
+++ api.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request
 from detector import Detector
+from pipeline import translate
+import easyocr
 
 
-model_en = Detector()
+model = Detector()
+ocr = easyocr.Reader(['en','ar'])
+
 
 app = Flask(__name__)
 
-@app.post('/predict')
+@app.post('/english')
 def english():
     data = request.json
     try:
@@ -14,13 +18,73 @@ def english():
     except KeyError:
         return jsonify({'error' : 'No text sent'})
         
-    predictions = model_en.predict(sample)
+    predictions = model.predict(sample)
 
     try:
         result = jsonify(predictions)
     except TypeError as e:
         result = jsonify({'error' : str(e)})
     return result
+
+@app.post('/arabic')
+def arabic():
+    data = request.json
+    try:
+        sample = data['text']
+        sample = translate(sample)
+    except KeyError:
+        return jsonify({'error' : 'No text sent'})
+    
+    predictions = model.predict(sample)
+
+    try:
+        result = jsonify(predictions)
+    except TypeError as e:
+        result = jsonify({'error' : str(e)})
+    return result
+
+@app.post('/english-image')
+def english_image():
+    data = request.files['img']
+    try:
+        extracted_text = ocr.readtext(data.stream.read(), detail=0, paragraph=True)
+    except KeyError:
+        return jsonify({'error' : 'No text sent'})
+        
+    text=''
+    for i in extracted_text:
+        text+=i
+    
+    predictions = model.predict(text)
+
+    try:
+        result = jsonify(predictions)
+    except TypeError as e:
+        result = jsonify({'error' : str(e)})
+    return result
+
+
+@app.post('/arabic-image')
+def arabic_image():
+    data = request.files['img']
+    try:
+        extracted_text = ocr.readtext(data.stream.read(), detail=0, paragraph=True)
+    except KeyError:
+        return jsonify({'error' : 'No text sent'})
+        
+    text=''
+    for i in extracted_text:
+        text+=i
+    
+    text = translate(text)
+    predictions = model.predict(text)
+    
+    try:
+        result = jsonify(predictions)
+    except TypeError as e:
+        result = jsonify({'error' : str(e)})
+    return result
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,6 +1,15 @@
 from pipeline import clean
 from pipeline import tokenizer
 import pickle
+from translate import Translator
+
 
 with open("pipeline/encoder", "rb") as f: 
     encoder = pickle.load(f)
+
+
+translator = Translator(from_lang='arabic', to_lang='english')
+
+def translate(text):
+    return translator.translate(text)
+
