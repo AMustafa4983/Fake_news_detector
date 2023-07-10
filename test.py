90bb86ef67812a5356c072cccc042a3495eea101
Abdelrahman Mustafa <Abdelrahman4983@gmail.com>
2023-07-10 15:33:01 +0300

    Model Updated to checkpoint_02 with [.3,91%]

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 
-url = 'http://127.0.0.1:5000/english'
+url = 'http://127.0.0.1:5000/predict'
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 
 data = {
-    'text': 'We have been cooling down for the past 4000 years”; the Earth has cooled since the ‘medieval warming’, “It’s all about when you start the measurements'
-}
+    'text':'يشير تقرير الهيئة الحكومية الدولية المعنية بتغير المناخ لعام 2021 بشأن العلوم الفيزيائية إلى أن تغير المناخ واسع النطاق وسريع ومكثف، ويؤكد على الضرورة الملحة لإجراء تخفيضات قوية ومستدامة في انبعاثات غازات الدفيئة.'
+    }
 
 response = requests.post(url, headers=headers, json=data)
 
